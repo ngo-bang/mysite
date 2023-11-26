ef02d8b1a2b05646ee5882dbcad26099e53d1170
ngo-bang <ngocongbang.ptit@gmail.com>
2023-11-26 16:52:05 +0700

    3

--- mqtt_publisher/views.py
+++ mqtt_publisher/views.py
@@ -2,23 +2,82 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import paho.mqtt.client as mqtt
 
+# setting callbacks for different events to see if it works, print the message etc.
+def on_connect(client, userdata, flags, rc, properties=None):
+    """
+        Prints the result of the connection with a reasoncode to stdout ( used as callback for connect )
 
-def publish_mqtt_message(topic, message):
-    client = mqtt.Client()
-    client.username_pw_set("btliot", "Bang1234")
-    client.connect("8f7725991cd94c77a6a40c759d55192c.s2.eu.hivemq.cloud", 8883)  # Replace with your MQTT broker's address and port
+        :param client: the client itself
+        :param userdata: userdata is set when initiating the client, here it is userdata=None
+        :param flags: these are response flags sent by the broker
+        :param rc: stands for reasonCode, which is a code for the connection result
+        :param properties: can be used in MQTTv5, but is optional
+    """
+    print("CONNACK received with code %s." % rc)
 
-    client.publish(topic, message)
 
-    client.disconnect()
+# with this callback you can see if your publish was successful
+def on_publish(client, userdata, mid, properties=None):
+    """
+        Prints mid to stdout to reassure a successful publish ( used as callback for publish )
+
+        :param client: the client itself
+        :param userdata: userdata is set when initiating the client, here it is userdata=None
+        :param mid: variable returned from the corresponding publish() call, to allow outgoing messages to be tracked
+        :param properties: can be used in MQTTv5, but is optional
+    """
+    print("mid: " + str(mid))
+
+
+# print which topic was subscribed to
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    """
+        Prints a reassurance for successfully subscribing
+
+        :param client: the client itself
+        :param userdata: userdata is set when initiating the client, here it is userdata=None
+        :param mid: variable returned from the corresponding publish() call, to allow outgoing messages to be tracked
+        :param granted_qos: this is the qos that you declare when subscribing, use the same one for publishing
+        :param properties: can be used in MQTTv5, but is optional
+    """
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+# print message, useful for checking if it was successful
+def on_message(client, userdata, msg):
+    """
+        Prints a mqtt message to stdout ( used as callback for subscribe )
+
+        :param client: the client itself
+        :param userdata: userdata is set when initiating the client, here it is userdata=None
+        :param msg: the message with topic and payload
+    """
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
 
 def publish_mqtt(request):
-    topic = "my/topic"
+    topic = "test"
     message = "Hello, MQTT!"
 
-    publish_mqtt_message(topic, message)
+    client = mqtt.Client(client_id="", userdata=None, protocol=mqtt.MQTTv5)
+    client.on_connect = on_connect
 
-    return HttpResponse("MQTT message published successfully!")
-#hivemq.webclient.1700930780721
-#IMi.bm6!O,$73RFna1Dh
+    # enable TLS for secure connection
+    client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+    # set username and password
+    client.username_pw_set("btliot", "Bang1234")
+    # connect to HiveMQ Cloud on port 8883 (default for MQTT)
+    client.connect("efbeb273d1874dceb6240a54458171e9.s1.eu.hivemq.cloud", 8883)
+
+    # setting callbacks, use separate functions like above for better visibility
+    client.on_subscribe = on_subscribe
+    client.on_message = on_message
+    client.on_publish = on_publish
+
+    # subscribe to all topics of encyclopedia by using the wildcard "#"
+    client.subscribe("encyclopedia/#", qos=1)
+
+    # a single publish, this can also be done in loops, etc.
+    client.publish(topic, payload=message, qos=1)
+
+    return HttpResponse(topic+" "+message+"MQTT message published successfully!")
